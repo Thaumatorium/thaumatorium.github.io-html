08761a1c295600ef8ff473f18f214a2e7794af15
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2020-05-09 23:15:21 +0200

    add more todo items; add slugify project

---
+++ projects/slugify.mjs
@@ -0,0 +1,15 @@
+console.log("running slugify.mjs")
+let input = document.getElementById("in");
+let output = document.getElementById("out");
+
+input.addEventListener('input', () => {
+  output.value = input.value.replace(/[^a-z]/gi, c => {
+    return "-";
+  });
+  output.value = output.value.toLowerCase();
+});
+
+output.addEventListener('click', () => {
+  output.select();
+  document.execCommand('copy');
+});

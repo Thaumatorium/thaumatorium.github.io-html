72d527ee7d3e0c58412a520800d82bfa59dd4129
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2020-05-11 00:39:54 +0200

    add base65 project

---
+++ projects/base64.mjs
@@ -0,0 +1,25 @@
+console.log("running base64.mjs")
+let in_encode = document.getElementById("in_encode");
+let out_encode = document.getElementById("out_encode");
+let in_decode = document.getElementById("in_decode");
+let out_decode = document.getElementById("out_decode");
+
+in_encode.addEventListener('input', () => {
+  // Ascii to Base64
+  out_encode.value = window.btoa(in_encode.value);
+});
+
+out_encode.addEventListener('click', () => {
+  out_encode.select();
+  document.execCommand('copy');
+});
+
+in_decode.addEventListener('input', () => {
+  // Base64 to Ascii
+  out_decode.value = window.atob(in_decode.value);
+});
+
+out_decode.addEventListener('click', () => {
+  out_decode.select();
+  document.execCommand('copy');
+});

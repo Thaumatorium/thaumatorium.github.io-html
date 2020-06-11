6a06ec48946070a74e2705d7b212d88a3711fb2c
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2020-06-11 22:59:58 +0200

    fix sw.js
    there was a double entry

--- sw.js
+++ sw.js
@@ -51,13 +51,12 @@ self.addEventListener('install', function(event) {
 				'/pages/wc5.png',
 			
 				// Projects
-				'/projects/scripts/starfield.mjs',
 				'/projects/scripts/base64.mjs',
 				'/projects/scripts/mstg.mjs',
 				'/projects/scripts/rot13.mjs',
 				'/projects/scripts/slugify.mjs',
 				'/projects/scripts/starfield.mjs',
-				
+
 				// CSS
 				'/styles/footer.css',
 				'/styles/header.css',

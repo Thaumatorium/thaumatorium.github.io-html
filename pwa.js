df45d418f6a9a7c4db36a73d4e20bf04f50cf358
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2020-07-03 23:55:18 +0200

    remove reduntant <script> elements (and code)
    Turns out I forgot to remove document.write for 404.html...
    Another reason to use a script file instead.

--- pwa.js
+++ pwa.js
@@ -13,28 +13,29 @@ if ('serviceWorker' in navigator) {
 	});
 }
 
+/*************************************************************
+* Fill in the current year for (c) at the bottom of the page *
+*************************************************************/
 let ty = document.getElementById("target-year");
 ty.innerHTML = new Date().getFullYear() + 10000;
 
+/************************************************************************
+* Filling in of "Last updated:" updated stuff at the bottom of the page *
+************************************************************************/
+let updated = document.getElementById("updated");
+let date = new Date(document.lastModified);
+updated.innerHTML = date.toISOString().split("T")[0];
+
+// Ssssh, don't tell anyone!
 const specialGoto = (event) => {
 	if (event.ctrlKey && event.shiftKey) {
 		window.location.href = "/pages/praetorianpi.html";
 	}
 }
 
-if (getCookie('cookie-notification') === "read") {
-	let notification = document.getElementsByClassName("cookie-notification")[0]
-	notification.parentNode.removeChild(notification);
-	console.log("cookie notification removed");
-} else {
-	let notification = document.getElementsByClassName("cookie-notification")[0]
-	notification.addEventListener("click", () => {
-		setCookie('cookie-notification', 'read', 365);
-		let notification = document.getElementsByClassName("cookie-notification")[0]
-		notification.parentNode.removeChild(notification);
-	});
-}
-
+/***********************
+* Cookie related stuff *
+***********************/
 function getCookie(name) {
 	return document.cookie
 		.split("; ")
@@ -50,4 +51,18 @@ function setCookie(name, value, exdays = 365) {
 
 function getCookieValue(name) {
 	return getCookie(name)[1];
+}
+
+// remove cookie notification if the user already accepted the notification
+if (getCookie('cookie-notification') === "read") {
+	let notification = document.getElementsByClassName("cookie-notification")[0]
+	notification.parentNode.removeChild(notification);
+	console.log("cookie notification removed");
+} else {
+	let notification = document.getElementsByClassName("cookie-notification")[0]
+	notification.addEventListener("click", () => {
+		setCookie('cookie-notification', 'read', 365);
+		let notification = document.getElementsByClassName("cookie-notification")[0]
+		notification.parentNode.removeChild(notification);
+	});
 }

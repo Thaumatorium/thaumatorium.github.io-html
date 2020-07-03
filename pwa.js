8b086f6e86a29b6e1069f11a2eade7a3f1e382da
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2020-07-03 23:40:36 +0200

    add cookie notification

--- pwa.js
+++ pwa.js
@@ -12,3 +12,42 @@ if ('serviceWorker' in navigator) {
 		console.log('Registration failed with ' + error);
 	});
 }
+
+let ty = document.getElementById("target-year");
+ty.innerHTML = new Date().getFullYear() + 10000;
+
+const specialGoto = (event) => {
+	if (event.ctrlKey && event.shiftKey) {
+		window.location.href = "/pages/praetorianpi.html";
+	}
+}
+
+if (getCookie('cookie-notification') === "read") {
+	let notification = document.getElementsByClassName("cookie-notification")[0]
+	notification.parentNode.removeChild(notification);
+	console.log("cookie notification removed");
+} else {
+	let notification = document.getElementsByClassName("cookie-notification")[0]
+	notification.addEventListener("click", () => {
+		setCookie('cookie-notification', 'read', 365);
+		let notification = document.getElementsByClassName("cookie-notification")[0]
+		notification.parentNode.removeChild(notification);
+	});
+}
+
+function getCookie(name) {
+	return document.cookie
+		.split("; ")
+		.map((cookie) => cookie.split("="))
+		.filter((cookie) => cookie[0] == name);
+}
+
+function setCookie(name, value, exdays = 365) {
+	let d = new Date(Date.now() + exdays * 24 * 60 * 60 * 1000);
+	let expires = "expires=" + d.toISOString();
+	document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookieValue(name) {
+	return getCookie(name)[1];
+}

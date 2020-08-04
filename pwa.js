e67ed7b3e3e3dd425ae1a3754e1b464e26ac819f
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2020-08-04 22:01:15 +0200

    reduce size of pwa.js; cleanup pwa.js
    There was some old code I'm not using for a while now, so I removed it.
    I also cleaned up some code, because it looked messy. I used closure compiler to minimize the code abit, without making a complete mess.

--- pwa.js
+++ pwa.js
@@ -1,40 +1,8 @@
-const CONSOLE_STYLE = "background: #66023C; color: #fff; padding: 2px";
+// Fill in the current year for (c) at the bottom of the page
+document.getElementById("target-year").innerHTML = (new Date).getFullYear() + 1E4;
 
-// This script installs the service worker that turns the website into a basic PWA
-// if ('serviceWorker' in navigator) {
-// 	navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(reg => {
-// 		if (reg.installing) {
-// 			console.info('%cpwa.js: Service worker installing', CONSOLE_STYLE);
-// 		} else if (reg.waiting) {
-// 			console.info('%cpwa.js: Service worker installed', CONSOLE_STYLE);
-// 		} else if (reg.active) {
-// 			console.info('%cpwa.js: Service worker active', CONSOLE_STYLE);
-// 		}
-// 	}).catch(error => {
-// 		console.info(`%cpwa.js: Registration failed with ${error}`, CONSOLE_STYLE);
-// 	});
-// }
-
-// Unregister all serviceworkers - I want to test the site without PWA functionality
-navigator.serviceWorker.getRegistrations().then(registrations => {
-	for (let registration of registrations) {
-		registration.unregister();
-	}
-});
-
-
-/*************************************************************
-* Fill in the current year for (c) at the bottom of the page *
-*************************************************************/
-let ty = document.getElementById("target-year");
-ty.innerHTML = new Date().getFullYear() + 10000;
-
-/************************************************************************
-* Filling in of "Last updated:" updated stuff at the bottom of the page *
-************************************************************************/
-let updated = document.getElementById("updated");
-let date = new Date(document.lastModified);
-updated.innerHTML = date.toISOString().split("T")[0];
+// Filling in of "Last updated:" updated stuff at the bottom of the page
+document.getElementById("updated").innerHTML = new Date(document.lastModified).toISOString().split("T")[0];
 
 // Ssssh, don't tell anyone!
 const specialGoto = event => {
@@ -43,39 +11,14 @@ const specialGoto = event => {
 	}
 }
 
-/***********************
-* Cookie related stuff *
-***********************/
-const getCookie = name => {
-	return document.cookie
-		.split("; ")
-		.map((cookie) => cookie.split("="))
-		.filter((cookie) => cookie[0] == name)[0];
-}
-
-const setCookie = (name, value, exdays = 365) => {
-	let d = new Date(Date.now() + exdays * 24 * 60 * 60 * 1000);
-	let expires = "expires=" + d.toUTCString();
-	document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-const cookieExists = name => {
-	return getCookie(name) !== undefined
-}
-const getCookieValue = name => {
-	return cookieExists(name) ? getCookie(name)[1] : [];
-}
-
 // remove cookie notification if the user already accepted the notification
-let notification = document.getElementById("cookie-notification");
-if (notification !== undefined) {
-	if (getCookieValue('cookie-notification') === "read") {
-		notification.parentNode.removeChild(notification);
-		console.info("cookie notification removed");
-	} else {
-		notification.addEventListener("click", () => {
-			setCookie('cookie-notification', 'read', 365);
-			notification.parentNode.removeChild(notification);
-		});
-	}
-}
+let cn = document.getElementById("cookie-notification");
+// This && is basically a shorter version of if/else, because closure compiler can't handle the ?. (Null Propagation) operator >:(
+null !== cn && (
+	("read" === document.cookie.split("; ").map(str => str.split("=")).reduce((acc, val) => ("cookie-notification" === val[0] ? val[1] : ""))) ?
+		cn.remove() :
+		cn.addEventListener("click", () => {
+			document.cookie = `cookie-notification=read;expires=${(new Date(Date.now() + 31536E6)).toUTCString()};path=/`;
+			cn.remove();
+		})
+)

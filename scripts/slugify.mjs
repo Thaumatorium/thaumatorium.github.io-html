fc1c043687f561189a55804e4320fa02c0245771
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2020-08-07 13:40:18 +0200

    slugify now reduces the amount of dashes (-)

--- scripts/slugify.mjs
+++ scripts/slugify.mjs
@@ -2,9 +2,7 @@ let input = document.getElementById("slugify_in");
 let output = document.getElementById("slugify_out");
 
 input.addEventListener('input', () => {
-	output.value = input.value.replace(/[^a-z0-9]/gi, c => {
-		return "-";
-	});
+	output.value = input.value.replace(/[^a-z0-9]/gi, _ => "-").replace(/(---*)+/gi, _ => "-");
 	output.value = output.value.toLowerCase();
 });
 

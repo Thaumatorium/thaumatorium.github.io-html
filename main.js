42a00b68e30c094fc3802a1f9b4a553f8c92a41e
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2020-08-16 21:35:29 +0200

    wrap code in an adEventListener 'load' because the JS ran too earyl
    There's a first time for anything, eh?

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-// Fill in the current year for (c) at the bottom of the page
-document.getElementById("target-year").innerHTML = (new Date).getFullYear() + 1E4;
+window.addEventListener('load', () => {
+	// Fill in the current year for (c) at the bottom of the page
+	document.getElementById("target-year").innerHTML = (new Date).getFullYear() + 1E4;
 
-// Filling in of "Last updated:" updated stuff at the bottom of the page
-document.getElementById("updated").innerHTML = new Date(document.lastModified).toISOString().split("T")[0];
+	// Filling in of "Last updated:" updated stuff at the bottom of the page
+	document.getElementById("updated").innerHTML = new Date(document.lastModified).toISOString().split("T")[0];
+
+	// remove cookie notification if the user already accepted the notification
+	let cn = document.getElementById("cookie-notification");
+	// This && is basically a shorter version of if/else, because closure compiler can't handle the ?. (Null Propagation) operator >:(
+	null !== cn && (
+		("read" === document.cookie.split("; ").map(str => str.split("=")).reduce((acc, val) => acc + ("cookie-notification" === val[0] ? val[1] : ""), "")) ?
+			cn.remove() :
+			document.querySelector("#cookie-notification>p>button").addEventListener("click", () => {
+				document.cookie = `cookie-notification=read;expires=${(new Date(Date.now() + 31536E6)).toUTCString()};path=/`;
+				cn.remove();
+			})
+	);
+});
 
 // Sssh - don't tell anyone!
 const specialGoto = event => {
@@ -10,18 +24,6 @@ const specialGoto = event => {
 		window.location.href = "/praetorianpi";
 	}
 }
-
-// remove cookie notification if the user already accepted the notification
-let cn = document.getElementById("cookie-notification");
-// This && is basically a shorter version of if/else, because closure compiler can't handle the ?. (Null Propagation) operator >:(
-null !== cn && (
-	("read" === document.cookie.split("; ").map(str => str.split("=")).reduce((acc, val) => acc + ("cookie-notification" === val[0] ? val[1] : ""), "")) ?
-		cn.remove() :
-		document.querySelector("#cookie-notification>p>button").addEventListener("click", () => {
-			document.cookie = `cookie-notification=read;expires=${(new Date(Date.now() + 31536E6)).toUTCString()};path=/`;
-			cn.remove();
-		})
-);
 
 // polyfill rel=preload
 (w => {

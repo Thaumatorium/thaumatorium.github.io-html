edf2005457ff5e1f92f8e7942f0135bea81fad8a
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2020-08-28 03:26:47 +0200

    move code out of onload trigger
    Hopefully code will now work again!

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-window.addEventListener('load', () => {
-	// Fill in the current year for (c) at the bottom of the page
-	document.getElementById("target-year").innerHTML = (new Date).getFullYear() + 1E4;
+// Fill in the current year for (c) at the bottom of the page
+document.getElementById("target-year").innerHTML = (new Date).getFullYear() + 1E4;
 
-	// Filling in of "Last updated:" updated stuff at the bottom of the page
-	document.getElementById("updated").innerHTML = new Date(document.lastModified).toISOString().split("T")[0];
+// Filling in of "Last updated:" updated stuff at the bottom of the page
+document.getElementById("updated").innerHTML = new Date(document.lastModified).toISOString().split("T")[0];
 
-	// remove cookie notification if the user already accepted the notification
-	let cn = document.getElementById("cookie-notification");
-	// This && is basically a shorter version of if/else, because closure compiler can't handle the ?. (Null Propagation) operator >:(
-	if (null !== cn) {
-		if ("read" === document.cookie.split("; ").map(str => str.split("=")).reduce((acc, val) => acc + ("cookie-notification" === val[0] ? val[1] : ""), "")) {
+// remove cookie notification if the user already accepted the notification
+let cn = document.getElementById("cookie-notification");
+// This && is basically a shorter version of if/else, because closure compiler can't handle the ?. (Null Propagation) operator >:(
+if (null !== cn) {
+	if ("read" === document.cookie.split("; ").map(str => str.split("=")).reduce((acc, val) => acc + ("cookie-notification" === val[0] ? val[1] : ""), "")) {
+		cn.remove();
+	} else {
+		cn.style.display = "";
+		document.querySelector("#cookie-notification>p>button").addEventListener("click", () => {
+			document.cookie = `cookie-notification=read;expires=${(new Date(Date.now() + 31536E6)).toUTCString()};path=/`;
 			cn.remove();
-		} else {
-			cn.style.display = "";
-			document.querySelector("#cookie-notification>p>button").addEventListener("click", () => {
-				document.cookie = `cookie-notification=read;expires=${(new Date(Date.now() + 31536E6)).toUTCString()};path=/`;
-				cn.remove();
-			})
-		}
+		})
 	}
-});
+}
 
 // Sssh - don't tell anyone!
 const specialGoto = event => {
